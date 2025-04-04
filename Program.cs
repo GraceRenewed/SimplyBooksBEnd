fac82551ee4be4ef0938f1621ab7e48a766a0704
Christina Vieau <cvieau@live.com>
2025-04-04 12:39:16 -0400

    configured web API to use EF Core

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using SimplyBooksBEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Http.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// allows passing datetimes without time zone data 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+// allows our api endpoints to access the database through Entity Framework Core
+builder.Services.AddNpgsql<SimplyBooksBEndDbContext>(builder.Configuration["SimplyBooksBEndDbConnectionString"]);
+
+// Set the JSON serializer options
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
